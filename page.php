175fd887408ed210928005b51ed5d395ff8c1a2d
Cass <codingcass@gmail.com>
2024-03-20 21:32:36 -0700

    Renamed to 'Pilot'

--- page.php
+++ page.php
@@ -7,11 +7,10 @@ get_header(); ?>
 		
 		<!-- main-column -->
 		<div class="main-column">
-
 			<?php if (have_posts()) :
 				while (have_posts()) : the_post();
 
-				get_template_part('content', get_post_format());
+				get_template_part('content', 'page');
 
 				endwhile;
 
@@ -20,7 +19,6 @@ get_header(); ?>
 
 				endif;
 				?>
-
 		</div><!-- /main-column -->
 
 		<?php get_sidebar(); ?>
--- search.php
+++ search.php
@@ -1,14 +1,20 @@
 <?php
 
 get_header(); ?>
-	
+
 	<!-- site-content -->
 	<div class="site-content clearfix">
 		
 		<!-- main-column -->
 		<div class="main-column">
+			
+			<?php
+			
+			if (have_posts()) : ?>
 
-			<?php if (have_posts()) :
+				<h2>Search results for: <?php the_search_query(); ?></h2>
+
+				<?php
 				while (have_posts()) : the_post();
 
 				get_template_part('content', get_post_format());
@@ -19,14 +25,15 @@ get_header(); ?>
 					echo '<p>No content found</p>';
 
 				endif;
-				?>
-
+			
+			?>
+			
 		</div><!-- /main-column -->
-
+		
 		<?php get_sidebar(); ?>
 		
 	</div><!-- /site-content -->
-	
-	<?php get_footer();
+
+<?php get_footer();
 
 ?>
